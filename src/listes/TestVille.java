85655894881d73a3583ed9a1b8dc6bae46a2de0a
NicolasSoulay <soulaynicolas@gmail.com>
2023-05-23 11:38:47 +0200

    fin tp 10

--- src/listes/TestVille.java
+++ src/listes/TestVille.java
@@ -12,5 +12,34 @@ public class TestVille {
         villes.add(new Ville("Pau", 77200));
         villes.add(new Ville("Marseille", 850700));
         villes.add(new Ville("Tarbes", 40600));
+
+        int a = villes.get(0).nbrHabitants;
+        int b = 0;
+
+        for (int i=0; i<villes.size(); i++ ) {
+            if (villes.get(i).nbrHabitants > a) {
+                a = villes.get(i).nbrHabitants;
+                b = i;
+            }
+        }
+        System.out.println("la ville la plus peuplé est: "+villes.get(b).nom);
+
+        for (int i=0; i<villes.size(); i++ ) {
+            if (villes.get(i).nbrHabitants < a) {
+                a = villes.get(i).nbrHabitants;
+                b = i;
+            }
+        }
+        System.out.println("la ville supprimée est: "+villes.get(b).nom);
+        villes.remove(b);
+
+        for (int i=0; i<villes.size(); i++ ) {
+            if (villes.get(i).nbrHabitants > 100000) {
+                villes.get(i).nom = villes.get(i).nom.toUpperCase();
+            }
+        }
+        for (Ville ville : villes) {
+            System.out.println(ville.nom);
+        }
     }
 }
